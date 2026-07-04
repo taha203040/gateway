37a50f3f980bd0f235b8f7849bf7405a347b4bc1
taha <jokerdz380@gmail.com>
2026-07-04 23:56:48 +0100

    inventory and checkout smoke test

---
+++ inventory/src/tests/smoke.test.ts
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 3, // Key for Smoke test. Keep it at 2, 3, max 5 VUs
+  duration: '1m', // This can be shorter or just a few iterations
+};
+
+export default () => {
+  const urlRes = http.get('http://localhost:4100');
+  check(urlRes, { 'status returned 200': (r) => r.status == 200 })
+  sleep(1);
+  // MORE STEPS
+  // Here you can have more steps or complex script
+  // Step1
+  // Step2
+  // etc.
+};
